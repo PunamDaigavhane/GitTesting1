95d5deef0c0d9453ab2fcc35f5ae149c68082201
PunamDaigavhane <punamdaigavhane74@gmail.com>
2024-02-04 13:56:20 +0530

    add new class

---
+++ GitTesting/src/com/testing/git/Test.java
@@ -0,0 +1,19 @@
+package com.testing.git;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		//Swapping
+		
+		x = x+y;
+		y = x-y;
+		x = x-y;
+		
+		System.out.println(x );
+		System.out.println(y);
+		
+	}
+
+}
